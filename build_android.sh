69f87349c02f697433fccd94f6016b87e86495d3
Smart <startechh030@gmail.com>
2026-07-25 03:04:05 +0100

    Replace old project

--- build_android.sh
+++ build_android.sh
@@ -16,7 +16,7 @@ build_abi() {
     cmake -B "build/$abi" \
         -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
         -DANDROID_ABI="$abi" -DANDROID_PLATFORM=android-26 \
-        -DANDROID_STL=c++_shared -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -G Ninja
+        -DANDROID_STL=c++_static -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -G Ninja
     cmake --build "build/$abi" --target obris_shared --parallel "$JOBS"
     mkdir -p "android/app/src/main/jniLibs/$abi"
     cp "build/$abi/libobris_shared.so" "android/app/src/main/jniLibs/$abi/"
